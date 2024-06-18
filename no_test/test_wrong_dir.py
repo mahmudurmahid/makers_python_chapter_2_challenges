edc91d00c7bfcf59171bd8f26880366a51984656
Edward Andress <eddie_andress@hotmail.com>
2024-06-18 15:52:14 +0100

    [SLUG] Transferring changes from Slug as of 2024-06-18 15:52:12 +0100.

--- no_test/test_wrong_dir.py
+++ no_test/test_wrong_dir.py
@@ -6,10 +6,10 @@ MESSAGE = """
     You are running `pytest` in the wrong directory.
     If you are trying to run the drills tests, do this:
     ; cd drills
-    ; pipenv run pytest -x
+    ; pytest -x
     If you are trying to run the program tests, do this:
     ; cd program
-    ; pipenv run pytest -x
+    ; pytest -x
 """
 
 
